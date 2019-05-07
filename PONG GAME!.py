cbcc48516a8f01278c9970ce1b8f1e50ecc87c55
Soumil Khandelwal <31521503+soumilk@users.noreply.github.com>
2019-05-07 22:55:17 +0530

    Added functionality of scores

--- PONG GAME!.py
+++ PONG GAME!.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from tkinter import messagebox
 import time
 
 root=Tk()
@@ -9,10 +10,15 @@ root.wm_attributes("-topmost",1)
 canvas=Canvas(root,width=600, height=500, bd=1,highlightthickness=1)
 canvas.config(bg="black")
 canvas.pack()
+l=canvas.create_text(300,20,font=("Arial",20,"bold"),text=" : ",fill="white")
+
 root.update()
 canvas.create_line(300,0,300,600,fill="white")
 
 class Ball:
+       def update(self):
+              l.configure(text=str(self.score1)+' : '+str(self.score2))
+       
        def __init__(self,canvas,paddle1,paddle2,color):
               self.paddle1=paddle1
               self.paddle2=paddle2
@@ -38,8 +44,16 @@ class Ball:
                      self.y=-4
               if pos[0]<=0:
                      self.x=4
+                     self.score1+=1
+                     print(self.score1)
+                     canvas.itemconfigure(l,text=str(self.score1)+" : "+str(self.score2))
+                     #update()
               if pos[2]>=self.canvas_width:
                      self.x=-4
+                     self.score2+=1
+                     print(self.score2)
+                     canvas.itemconfigure(l,text=str(self.score1)+" : "+str(self.score2))
+                     #update()
               if self.hit_paddle1(pos)==True:
                      self.x=4
               if self.hit_paddle2(pos)==True:
@@ -115,6 +129,9 @@ paddle2=Paddle2(canvas,"lightgreen")
 ball=Ball(canvas,paddle1,paddle2,"yellow")
 
 while 1:
+       if ball.score1==10 or ball.score2==10:
+              messagebox.showinfo("Game Over","Player 1 ="+str(ball.score1)+" Player 2 ="+str(ball.score2))
+              break
        ball.draw()
        paddle1.draw()
        paddle2.draw()
